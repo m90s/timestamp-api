0ecd01fdb59d9db18073c714ab6975c921f90631
Mohammed alsokait <malsokait@gmail.com>
2016-09-29 02:23:54 +0000

    Finished time api

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-var express = require("express")
+var express = require("express");
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello22 World!');
+app.get('/:query', function (req, res) {
+  var query = req.params.query;
+  var date;
+  var jsonResponse = {'unix': null, 'natural': null};
+  
+  if(parseInt(query)){
+      date = parseUnixQuery(parseInt(query));
+      console.log(typeof date);
+  }
+  else {
+      date = parseStringQuery(query);
+  }
+  
+  if(date){
+      jsonResponse.unix = date.getTime();
+      jsonResponse.natural = date.toDateString();
+  }
+  
+  res.json(jsonResponse);
+  
 });
 
+function parseUnixQuery(query){
+    var date = new Date(query);
+    return date;
+}
+
+function parseStringQuery(query) {
+    var date = new Date(Date.parse(query));
+    return date;
+}
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Running on 8080');
 });
